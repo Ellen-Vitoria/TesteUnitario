6a925145301f8dac3411037cd194dd806400a9fc
ellen <ellen.vitoria.ogs@gmail.com>
2021-11-03 20:08:26 -0300

    Adicionando classe de Teste

--- test/CalculadoraTest.java
+++ test/CalculadoraTest.java
@@ -1,4 +1,22 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculadoraTest {
-  
+
+    @Test
+    void testaAdicao() {
+    }
+
+    @Test
+    void testaSubtracao() {
+    }
+
+    @Test
+    void testaMultiplicacao() {
+    }
+
+    @Test
+    void testaDivisao() {
+    }
 }
